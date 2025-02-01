b9f2b1900c1e47bb3473e34f35cda5d1b1d37ae2
rbrdlik <roman.brdlik@student.spsmb.cz>
2025-02-01 19:18:55 +0100

    [Client] Vytvoření a design stránky signIn & signUp

--- client/src/pages/SignIn/index.jsx
+++ client/src/pages/SignIn/index.jsx
@@ -1,9 +1,29 @@
-import "../../scss/signUp.scss";
+import { Link } from "react-router-dom";
+import SignInAndUp from "../../components/signInAndUp";
+
+// Import style
+import "../../scss/signInAndUp.scss";
 
 export default function SignIn(){
     return(
         <>
-            <h1 className="text">SignIn</h1>
+            <SignInAndUp>
+                <div className="form">
+                    <h1>Přihlásit se</h1>
+                    <form>
+                        <div className="input">
+                            <h4>Email</h4>
+                            <input type="email" placeholder="Zadejte svůj email"/>
+                        </div>
+                        <div className="input">
+                            <h4>Heslo</h4>
+                            <input type="password" placeholder="Zadejte své heslo"/>
+                        </div>
+                        <button>Přihlásit se</button>
+                    </form> 
+                    <p>Ještě nemáte účet? <Link to={"/sign-up"}>Zaregistrovat se</Link></p>
+                </div>
+            </SignInAndUp>
         </>
     )
 }
--- client/src/pages/SignUp/index.jsx
+++ client/src/pages/SignUp/index.jsx
@@ -1,9 +1,37 @@
-import "../../scss/signIn.scss";
+import { Link } from "react-router-dom";
+import SignInAndUp from "../../components/signInAndUp";
 
-export default function SignIn(){
+// Import style
+import "../../scss/signInAndUp.scss";
+
+export default function SignUp(){
     return(
         <>
-            <h1 className="text">SignUp</h1>
+            <SignInAndUp>
+                <div className="form">
+                    <h1>Zaregistrovat se</h1>
+                    <form>
+                        <div className="input">
+                            <h4>Uživatelské jméno</h4>
+                            <input type="text" placeholder="Vytvořte si uživatelské jméno"/>
+                        </div>
+                        <div className="input">
+                            <h4>Email</h4>
+                            <input type="email" placeholder="Zadejte svůj email"/>
+                        </div>
+                        <div className="input">
+                            <h4>Heslo</h4>
+                            <input type="password" placeholder="Zadejte své heslo"/>
+                        </div>
+                        <div className="input">
+                            <h4>Heslo znovu</h4>
+                            <input type="password" placeholder="Zadejte své heslo znovi"/>
+                        </div>
+                        <button>Zaregistrovat se</button>
+                    </form> 
+                    <p>Již máte účet? <Link to={"/sign-in"}>Přihlásit se</Link></p>
+                </div>
+            </SignInAndUp>
         </>
     )
 }
